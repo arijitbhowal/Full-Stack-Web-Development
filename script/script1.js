2abdde9eb5087af60961c8706e172599ae7a327c
arijitbhowal <arijitaeiou2002@gmail.com>
2023-09-05 11:40:36 +0530

    Commit

--- script/script1.js
+++ script/script1.js
@@ -25,7 +25,7 @@ document.getElementById("sendButton").addEventListener("click", function () {
     const projectDescription = document.getElementById("project_description").value;
 
     if (firstName === "") {
-        document.getElementById("firstnameError").textContent = "Please supply the details";
+        document.getElementById("firstnameError").textContent = "Please supply your first name";
         document.getElementById("firstname").parentNode.style.border = "0.5px solid red"; 
         document.getElementById("firstnameLabel").style.color = "red"; 
         document.getElementById("firstnameIcon").style.color = "red"; 
@@ -34,7 +34,7 @@ document.getElementById("sendButton").addEventListener("click", function () {
     }
 
     if (lastName === "") {
-        document.getElementById("lastnameError").textContent = "Please supply the details";
+        document.getElementById("lastnameError").textContent = "Please supply your last name";
         document.getElementById("lastname").parentNode.style.border = "0.5px solid red";
         document.getElementById("lastnameLabel").style.color = "red";
         document.getElementById("lastnameIcon").style.color = "red";
@@ -42,7 +42,7 @@ document.getElementById("sendButton").addEventListener("click", function () {
     }
 
     if (email === "") {
-        document.getElementById("emailError").textContent = "Please supply the details";
+        document.getElementById("emailError").textContent = "Please supply your email address";
         document.getElementById("email").parentNode.style.border = "0.5px solid red";
         document.getElementById("emailLabel").style.color = "red";
         document.getElementById("emailIcon").style.color = "red";
@@ -50,7 +50,7 @@ document.getElementById("sendButton").addEventListener("click", function () {
     }
 
     if (phone === "") {
-        document.getElementById("phoneError").textContent = "Please supply the details";
+        document.getElementById("phoneError").textContent = "Please supply your phone number";
         document.getElementById("phone").parentNode.style.border = "0.5px solid red";
         document.getElementById("phoneLabel").style.color = "red";
         document.getElementById("phoneIcon").style.color = "red";
@@ -58,7 +58,7 @@ document.getElementById("sendButton").addEventListener("click", function () {
     }
 
     if (address === "") {
-        document.getElementById("addressError").textContent = "Please supply the details";
+        document.getElementById("addressError").textContent = "Please supply your street address";
         document.getElementById("address").parentNode.style.border = "0.5px solid red";
         document.getElementById("addressLabel").style.color = "red";
         document.getElementById("addressIcon").style.color = "red";
@@ -66,7 +66,7 @@ document.getElementById("sendButton").addEventListener("click", function () {
     }
 
     if (city === "") {
-        document.getElementById("cityError").textContent = "Please supply the details";
+        document.getElementById("cityError").textContent = "Please supply your city";
         document.getElementById("city").parentNode.style.border = "0.5px solid red";
         document.getElementById("cityLabel").style.color = "red";
         document.getElementById("cityIcon").style.color = "red";
@@ -74,7 +74,7 @@ document.getElementById("sendButton").addEventListener("click", function () {
     }
 
     if (state === "Select a state...") {
-        document.getElementById("stateError").textContent = "Please select a state";
+        document.getElementById("stateError").textContent = "Please select your state";
         document.getElementById("state").parentNode.style.border = "0.5px solid red";
         document.getElementById("stateLabel").style.color = "red";
         document.getElementById("stateIcon").style.color = "red";
@@ -87,7 +87,7 @@ document.getElementById("sendButton").addEventListener("click", function () {
     }
 
     if (projectDescription === "") {
-        document.getElementById("projectDescriptionError").textContent = "Please supply the details";
+        document.getElementById("projectDescriptionError").textContent = "Please supply your project description";
         document.getElementById("project_description").parentNode.style.border = "0.5px solid red";
         document.getElementById("projectDescriptionLabel").style.color = "red";
         document.getElementById("projectDescriptionIcon").style.color = "red";
